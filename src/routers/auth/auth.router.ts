6d743d346675bd9009670c379c22dbe6a1df8b54
Edosicca <47154408+sichej@users.noreply.github.com>
2023-12-28 11:11:25 +0100

    Typo

--- src/routers/auth/auth.router.ts
+++ src/routers/auth/auth.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, register } from '../../controllers/auth/auth.constrollers';
+import { login, register } from '../../controllers/auth/auth.controllers';
 import { loginMiddleware } from '../../controllers/middlewares/auth.middleware';
 
 const authRouter = express.Router();
