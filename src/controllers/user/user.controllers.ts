833948067664aeae25192401d261d85fd0dbb640
Edosicca <47154408+sichej@users.noreply.github.com>
2023-12-28 11:46:47 +0100

    Fix wrong indentation

--- src/controllers/user/user.controllers.ts
+++ src/controllers/user/user.controllers.ts
@@ -4,14 +4,14 @@ import { HTTP_Codes } from '../../repository/httpCodes';
 
 export const getUserByEmailController = async (req: Request, res: Response): Promise<void> => {
     try {
-      const email: string = req.body.email;
-      const user = await findUserByEmail(email);
-      if (!user) {
+        const email: string = req.body.email;
+        const user = await findUserByEmail(email);
+    if (!user) {
         res.status(HTTP_Codes.NOT_FOUND).send({ message: 'User not found' });
         return;
-      }
-      res.status(HTTP_Codes.OK).send(user);
+    }
+        res.status(HTTP_Codes.OK).send(user);
     } catch (error) {
-      res.status(HTTP_Codes.INTERNAL_SERVER_ERROR).send({ error: error.message });
+        res.status(HTTP_Codes.INTERNAL_SERVER_ERROR).send({ error: error.message });
     }
 };
