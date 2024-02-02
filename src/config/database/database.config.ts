9d2a6824840040f79a6298f5cd01377abae6feb6
Edosicca <47154408+sichej@users.noreply.github.com>
2024-02-02 09:50:48 +0100

    Add Test and Prod differentiations

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -3,8 +3,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME_TEST, process.env.DB_USER_TEST, process.env.DB_PWD_TEST, {
-    host: process.env.DB_HOST_TEST,
+const isTestEnvironment = process.env.NODE_ENV === 'test';
+
+const dbName = isTestEnvironment ? process.env.DB_NAME_TEST : process.env.DB_NAME;
+const dbUser = isTestEnvironment ? process.env.DB_USER_TEST : process.env.DB_USER;
+const dbPwd = isTestEnvironment ? process.env.DB_PWD_TEST : process.env.DB_PWD;
+const dbHost = isTestEnvironment ? process.env.DB_HOST_TEST : process.env.DB_HOST;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPwd, {
+    host: dbHost,
     dialect: 'mariadb'
 });
 
